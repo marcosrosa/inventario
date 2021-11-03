f25eba7e14a31631a64b6acfaff357e07ba18bef
marcos <marcos@gaivotas>
2021-11-03 20:09:51 -0300

    exibe qtd total coletada e total geral de informatica

--- src/main/java/br/jus/jfes/sisgepi/inventario/controller/ResumoSetorBean.java
+++ src/main/java/br/jus/jfes/sisgepi/inventario/controller/ResumoSetorBean.java
@@ -26,7 +26,11 @@ public class ResumoSetorBean implements Serializable {
 	@Inject
     private SisgepiConsulta sisgepiBusca;
 	
-	private List<ResumoSetor> listResumoSetor;	
+	private List<ResumoSetor> listResumoSetor;
+	
+	private Long totalColetado;
+	
+	private Integer totalGeral = 3+327+285+6100+234;
 	
 	public ResumoSetorBean() {
 		// TODO Auto-generated constructor stub
@@ -35,21 +39,40 @@ public class ResumoSetorBean implements Serializable {
     public void onPostImportacao(@Observes(notifyObserver = Reception.IF_EXISTS) final Inventario invent) {
         iniciar();
     }    
-
 	
 	@PostConstruct
 	private void iniciar() {
 		this.listResumoSetor = sisgepiBusca.getQuantidadePorSetor(202100);
+		totalColetado = 0L;
+		if (listResumoSetor != null) {
+			listResumoSetor.forEach(r -> {
+				totalColetado += r.getQtd();
+			});
+		}
 	}
 	
-	
-
 	public List<ResumoSetor> getListResumoSetor() {
 		return listResumoSetor;
 	}
 
 	public void setListResumoSetor(List<ResumoSetor> listResumoSetor) {
 		this.listResumoSetor = listResumoSetor;
+	}
+
+	public Long getTotalColetado() {
+		return totalColetado;
+	}
+
+	public void setTotalColetado(Long totalColetado) {
+		this.totalColetado = totalColetado;
+	}
+
+	public Integer getTotalGeral() {
+		return totalGeral;
+	}
+
+	public void setTotalGeral(Integer totalGeral) {
+		this.totalGeral = totalGeral;
 	}
 
 }
